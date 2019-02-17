d8f34c70a13ef546667b71fb54215cb1b3addac0
Preet Dabre <dabre.preet@gmail.com>
2019-02-17 15:20:36 +0530

    One comment

---
+++ server/server.js
@@ -0,0 +1,39 @@
+var mongoose = require('mongoose');
+
+//tell mongoose to use javascript's own promise
+mongoose.promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+var Todo = mongoose.model('Todo', {
+    text: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    },
+    completedAt: {
+        type: Number
+    }
+});
+
+var newTodo = new Todo({
+    text: 'Cooked dinner'
+});
+
+// newTodo.save().then((doc) => {
+//     console.log('Saved doc', doc);
+// }, (e) => {
+//     console.log('Unable to save todo');
+// });
+
+var otherTodo = new Todo({
+    text: 'Buy clothes',
+    completed: false,
+    completedAt: 120
+});
+
+otherTodo.save().then((doc) => {
+    console.log(doc);
+}, (e) => {
+    console.log('Unable to save', e);
+});
