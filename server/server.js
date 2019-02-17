fafb0d693b659edc73d813914f4762a8d8b61137
Preet Dabre <dabre.preet@gmail.com>
2019-02-17 21:41:11 +0530

    Added GET /todos route

--- server/server.js
+++ server/server.js
@@ -23,13 +23,13 @@ app.post('/todos', (req, res) => {
 });
 
 
-// app.get('/todos', (req, res) => {
-//     Todo.find().then((todos) => {
-//         res.send({todos})
-//     }, (err) => {
-//         res.status(400).send(err);
-//     });
-// });
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos})
+    }, (err) => {
+        res.status(400).send(e);
+    });
+});
 
 
 app.listen(3000, () => {
