0b24b2c3cde35a5b058b0cbd18a0950793cbc38a
Preet Dabre <dabre.preet@gmail.com>
2019-02-18 21:49:29 +0530

    Queries playground

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,25 @@
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+
+var id = '6c6ac58856149a0ca7a393ea';
+
+Todo.find({
+    _id: id
+}).then((todos) => {
+    console.log('Todos', todos);
+});
+
+Todo.findOne({
+    _id: id
+}).then((todos) => {
+    console.log('Todo', todo);
+});
+
+Todo.findById(id).then((todo) => {
+    if (!todo) {
+        return console.log('Idnot found');
+    }
+    console.log('Todo by id', id);
+}).catch((e) => {
+    console.log(e);
+})
