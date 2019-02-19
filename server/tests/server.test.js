95a0700cb7b39d15d35ca9b0b381c77ef2b24c5d
Preet Dabre <dabre.preet@gmail.com>
2019-02-19 12:09:41 +0530

    Test the DELETE todos by id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -102,3 +102,42 @@ describe('GET /todos/:id', () => {
             .end(done);
     });
 });
+
+
+describe('DELETE /todos/:id', () => {
+    it('should remove a todo', (done) => {
+        var hexId = todos[1]._id.toHexString();
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(hexId);
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                Todo.findById(hexId).then((todo) => {
+                    expect(todo).toBeFalsy();
+                    done();
+                }).catch((err) => {
+                    done(err);
+                });
+            });
+    });
+
+    it('should return 404', (done) => {
+        request(app)
+            .delete(`/todos/${new ObjectId().toHexString()}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return 404 if ObjectId is invalid', (done) => {
+        request(app)
+            .get('/todos/123')
+            .expect(404)
+            .end(done);
+    });
+});
