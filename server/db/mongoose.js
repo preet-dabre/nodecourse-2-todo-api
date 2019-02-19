6066adc42b41eb73f53c0794e3e7911b5989f504
Preet Dabre <dabre.preet@gmail.com>
2019-02-19 14:40:33 +0530

    Setup diff test and dev environments

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 //tell mongoose to use javascript's own promise
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {
     mongoose
